ded69225510f60bb500ed42845812133454b4f4b
Lawrence Whiteside <law@cinemasetfree.com>
2018-05-02 21:32:00 -0700

    quick fix

--- config/webpack.dev-server.js
+++ config/webpack.dev-server.js
@@ -1,6 +1,5 @@
 const path = require("path")
 const webpack = require("webpack")
-const ExtractTextPlugin = require("extract-text-webpack-plugin")
 var nodeExternals = require("webpack-node-externals")
 
 module.exports = {
@@ -28,15 +27,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: {
-            loader: "css-loader",
-            options: {
-              minimize: true
-            }
-          }
-        })
+        use: "css-loader"
       },
       {
         test: /\.jpg$/,
@@ -61,7 +52,6 @@ module.exports = {
     ]
   },
   plugins: [
-    new ExtractTextPlugin("[name].css"),
     new webpack.DefinePlugin({
       "process.env": {
         NODE_ENV: JSON.stringify("development")
